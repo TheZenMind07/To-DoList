289ab393d3be8ca42dcc630dd02f602e08a07d4b
Rajkishor Pradhan <rajkp7598@gmail.com>
2021-03-02 01:00:02 +0530

    1. Added express routing

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const aboutContent =
 const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+let posts = [];
 const app = express();
 
 app.set("view engine", "ejs");
@@ -19,7 +20,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.render("home", { homeStartingContent: homeStartingContent });
+    console.log(posts);
+    res.render("home", { homeStartingContent: homeStartingContent, posts: posts });
+});
+
+app.get("/posts/:content", function (req, res) {
+    posts.forEach(post => {
+        if (post.title === req.params.content) {
+            console.log("Match Found");
+        }
+    });
+    console.log(req.params.content);
 });
 
 app.get("/about", function (req, res) {
@@ -39,6 +50,8 @@ app.post("/compose", function (req, res) {
         title: req.body.composeTitle,
         text: req.body.composeText
     };
+    posts.push(post);
+    res.redirect("/");
 });
 
 app.listen(3000, function () {
